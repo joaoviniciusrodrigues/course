c0d2eafd525ed9f85fc28c8ea594773d93297c79
João Vinicius <joaoviniciusj2012@gmail.com>
2023-01-05 21:53:28 -0300

    Vesão inicial

---
+++ src/main/java/com/ead/course/services/imp/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ead.course.services.imp;
+
+import com.ead.course.models.UserModel;
+import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.UserRepository;
+import com.ead.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Override
+    public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+        return userRepository.findAll(spec, pageable);
+    }
+
+    @Override
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    @Override
+    public void delete(UUID userId) {
+        courseRepository.deleteById(userId);
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+}
